6d2c25303ac45be62d73a022d0a7c7eeddc58a64
gohilparikshitsinh48 <gohilparikshitsinh48@gmail.com>
2025-09-06 19:06:24 +0530

    Create sumdivd_13.c

---
+++ sumdivd_13.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int sum = 0;
+
+    for (int i = 1; i <= 100; i++) {
+        if (i % 13 == 0) {
+            sum += i;
+        }
+    }
+
+    printf("Sum of numbers divisible by 13 is %d.\n", sum);
+
+    return 0;                                                    
+}
