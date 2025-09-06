0730c60a4da47db04bce8af53584887acab2c351
gohilparikshitsinh48 <gohilparikshitsinh48@gmail.com>
2025-09-06 18:49:22 +0530

    Create nameN.c

---
+++ nameN.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter how many times you want to print the name: ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) {
+        printf("Gohil Parikshitsinh\n");   
+    }
+
+    return 0;
+}
